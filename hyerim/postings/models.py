987a03ec090ec1d5ce7da971e5ebe5f1c5d9bc92
choihyerim <hyerimc858@gmail.com>
2021-07-28 21:51:50 +0900

    Add: postings westagram

--- hyerim/postings/models.py
+++ hyerim/postings/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+from users.models import User
+
 # Create your models here.
+
+class Post(models.Model):
+  creation_time   = models.DateTimeField()
+  image_url       = models.CharField(max_length=200)
+  posting_content = models.CharField(max_length=1000)
+  user_id         = models.ForeignKey(User, on_delete=models.CASCADE, related_name='users')
+
+  class Meta:
+    db_table = 'posts'
--- hyerim/postings/views.py
+++ hyerim/postings/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+import json
+from users.utils import signin_decorator
+
+from django.http  import JsonResponse
+from django.views import View
+from django.utils import timezone
+
+from users.models    import User
+from postings.models import Post
 
 # Create your views here.
+
+class PostView(View):
+  #@signin_decorator
+  def post(self, request):
+    try:
+      data = json.loads(request.body)
+
+      if not User.objects.get(email=data['email']).exists():
+        return JsonResponse({'message':'INVALID_USER'}, status=401)
+
+      now = timezone.now() 
+
+      Post.objects.create(
+        creation_time   = now.strftime("%H:%M:%S"),
+        image_url       = data['image_url'],
+        posting_title   = data['posting_title'],
+        posting_content = data['posting_content'],
+        user_id         = User.objects.get(email=data['email']).id  
+      )
+      return JsonResponse({'message':'CREATED'}, status=201)
+
+    except:
+      return JsonResponse({'message':'KEY_ERROR'}, status=400)
+
+  #@signin_decorator
+  #def get(self, request):
+    #results = []
+    #posts = Post.objects.all()
+
+    #for post in posts:
+      #results.append(
+        #{
+          #'user' : post.users.get(id = user_id)
+
+        #}
+      #)
+              
+      
+    #return JsonResponse({'result':results}, status=200)
